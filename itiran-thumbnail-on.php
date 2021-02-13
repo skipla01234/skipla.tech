e4576cc60e3fac90728e27759421b15943ab0a85
skipla01234 <office.worker.skill.plus@gmail.com>
2021-02-13 10:43:02 +0900

    一覧のカード表示を少し修正

--- itiran-thumbnail-on.php
+++ itiran-thumbnail-on.php
@@ -20,17 +20,16 @@
 				<img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" />
 			<?php } ?>
 		<?php endif; ?>
-        <span class="category-label"></span>
+        <span class="category-label">
+        <?php if ( isset( $GLOBALS['stdata465']) && $GLOBALS['stdata465'] === 'yes' ) :
+        	echo '<div class="st-catgroup-under">';
+        	get_template_part( 'st-single-category' ); //カテゴリー
+        	echo '</div>';
+        endif; //カテゴリー ?>
+        </span>
     </div>
 	<h3 class="kanren-detail-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    <p class="kanren-detail-text">
-				<?php get_template_part( 'st-excerpt' ); //抜粋 ?>
-				<?php if ( isset( $GLOBALS['stdata465']) && $GLOBALS['stdata465'] === 'yes' ) :
-					echo '<div class="st-catgroup-under">';
-					get_template_part( 'st-single-category' ); //カテゴリー
-					echo '</div>';
-				endif; //カテゴリー ?>
-    </p>
+	<?php get_template_part( 'st-excerpt' ); //抜粋 ?>
   </div>
   <?php endwhile; endif;?>
 </div>
