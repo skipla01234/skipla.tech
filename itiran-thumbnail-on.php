2c802f0bdb24f571698c194b687e291a77ab9771
skipla01234 <office.worker.skill.plus@gmail.com>
2021-02-13 10:02:57 +0900

    一覧表示をカード化してみた

--- itiran-thumbnail-on.php
+++ itiran-thumbnail-on.php
@@ -6,48 +6,31 @@
 	endif;
 	$st_infeed_count = 1;
 ?>
-<div class="kanren <?php st_marugazou_class(); //サムネイルを丸くする ?>">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php //インフィード広告
-			if( (is_active_sidebar( 26 ) && (trim($st_infeed) !== '')) && ($st_infeed_count % $st_infeed === 0) ){ ?>
-				<div class="st-infeed-adunit">
-					<?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 26 ) ) : else : ?>
-					<?php endif; ?>
-				</div>
-			<?php
-			}
-			$st_infeed_count ++;
-			?>
-		<dl class="clearfix">
-			<dt><a href="<?php the_permalink(); ?>">
-					<?php if ( has_post_thumbnail() ): // サムネイルを持っているときの処理 ?>
-						<?php get_template_part( 'st-thumbnail' ); //アイキャッチ画像 ?>
-					<?php else: // サムネイルを持っていないときの処理 ?>
-						<?php if(trim($GLOBALS['stdata97']) !== ''){ ?>
-							<img src="<?php echo esc_url( ($GLOBALS['stdata97']) ); ?>" alt="no image" title="no image" width="100" height="100" />
-						<?php }else{ ?>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" width="100" height="100" />
-						<?php } ?>
-					<?php endif; ?>
-				</a></dt>
-			<dd>
-				<?php if ( trim( $GLOBALS['stdata465']) === '' ) : get_template_part( 'st-single-category' ); endif; //カテゴリー ?>
-				<h3><a href="<?php the_permalink(); ?>">
-						<?php the_title(); ?>
-					</a></h3>
 
-				<?php get_template_part( 'itiran-date-tag' ); //投稿日 ?>
-
+<div class="kanren-card">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="kanren-detail">
+    <div class="kanren-detail-image">
+		<?php if ( has_post_thumbnail() ): // サムネイルを持っているときの処理 ?>
+			<?php get_template_part( 'st-thumbnail' ); //アイキャッチ画像 ?>
+		<?php else: // サムネイルを持っていないときの処理 ?>
+			<?php if(trim($GLOBALS['stdata97']) !== ''){ ?>
+				<img src="<?php echo esc_url( ($GLOBALS['stdata97']) ); ?>" alt="no image" title="no image" />
+			<?php } else { ?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" />
+			<?php } ?>
+		<?php endif; ?>
+        <span class="category-label"></span>
+    </div>
+	<h3 class="kanren-detail-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <p class="kanren-detail-text">
 				<?php get_template_part( 'st-excerpt' ); //抜粋 ?>
 				<?php if ( isset( $GLOBALS['stdata465']) && $GLOBALS['stdata465'] === 'yes' ) :
 					echo '<div class="st-catgroup-under">';
 					get_template_part( 'st-single-category' ); //カテゴリー
 					echo '</div>';
 				endif; //カテゴリー ?>
-			</dd>
-		</dl>
-	<?php endwhile;
-	else: ?>
-
-	<?php endif; ?>
+    </p>
+  </div>
+  <?php endwhile; endif;?>
 </div>
